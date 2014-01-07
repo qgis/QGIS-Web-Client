1ae51535e4d601d8f16f9185112065593889f69a
Bernhard Ströbl <bernhard.stroebl@jena.de>
2014-01-07 15:40:27 +0100

    Add ":" in authid definition

--- site/js/GlobalOptions.js
+++ site/js/GlobalOptions.js
@@ -135,7 +135,7 @@ var projectTitles = {
 };
 
 //EPSG projection code of your QGIS project
-var authid = "EPSG"+3857;
+var authid = "EPSG:"+3857;
 
 //background transparency for the QGIS server generated layer (commercial background layers not effected)
 //set to true if you want the background to be transparent, layer image will be bigger (32 vs 24bit)
