04e558f0b084ecf98b55bb86595362ad108e0ff3
Your Name <tudor.barascu@qtibia.ro>
2016-04-14 12:06:01 +0300

    fix Romania proj4js definition
    See https://github.com/OSGeo/proj.4/pull/366 for more details

--- site/libs/proj4js/defs/EPSG3844.js
+++ site/libs/proj4js/defs/EPSG3844.js
@@ -1,1 +1,1 @@
-Proj4js.defs["EPSG:3844"] = "+proj=sterea +lat_0=46 +lat_ts=46 +lon_0=25 +k=0.99975 +x_0=500000 +y_0=500000 +ellps=krass +towgs84=33.4,-146.6,-76.3,-0.359,-0.053,0.844,-0.84 +units=m +no_defs";
+Proj4js.defs["EPSG:3844"] = "+proj=sterea +lat_0=46 +lat_ts=46 +lon_0=25 +k=0.99975 +x_0=500000 +y_0=500000 +ellps=krass +towgs84=2.3287,-147.0425,-92.0802,-0.3092483,0.32482185,0.49729934,5.68906266 +units=m +no_defs";
